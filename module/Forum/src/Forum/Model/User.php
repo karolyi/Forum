cd4c54ca16f9e663fa19612e05b25f116eab779e
László Károlyi <laszlo@karolyi.hu>
2014-03-13 20:22:08 +0100

    Updated User model

--- module/Forum/src/Forum/Model/User.php
+++ module/Forum/src/Forum/Model/User.php
@@ -3,6 +3,7 @@
 namespace Forum\Model;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Rhumsaa\Uuid\Uuid;
 
 /** @ODM\Document(collection="users") */
 class User
@@ -11,10 +12,91 @@ class User
     private $id;
 
     /** @ODM\String */
-    private $name;
+    private $name = null;
 
     /** @ODM\String */
-    private $password;
+    private $password = null;
+
+    /** @ODM\String */
+    private $email = null;
+
+    /** @ODM\String */
+    private $otherEmails = null;
+
+    /** @ODM\String */
+    private $quote = null;
+
+    /** @ODM\String */
+    private $language = 'en_US';
+
+    /** @ODM\Int */
+    private $inviteSuccess = 0;
+
+    /** @ODM\Int */
+    private $maxPostsPerDay = 0;
+
+    /** @ODM\Int */
+    private $invitations = 0;
+
+    /** @ODM\Int */
+    private $reminders = 0;
+
+    /** @ODM\ReferenceOne(targetDocument="User", simple=true) */
+    private $inviterUser = null;
+
+    /** @ODM\Date */
+    private $regDate = null;
+
+    /** @ODM\String */
+    private $friendIntroduction = null;
+
+    /** @ODM\String */
+    private $regIntroduction = null;
+
+    /** @ODM\String */
+    private $introduction = null;
+
+    /** @ODM\String */
+    private $regId = null;
+
+    /**
+    * @ODM\String
+    * @ODM\Index(unique=true, order="asc")
+    */
+    private $slug = null;
+
+    /** @ODM\Int */
+    private $sumComments = 0;
+
+    /** @ODM\Int */
+    private $todayComments = 0;
+
+    /** @ODM\Int */
+    private $topicCommentsPerPage = 50;
+
+    /** @ODM\Int */
+    private $topicPerGroup = 50;
+
+    /** @ODM\Int */
+    private $voteLimit = -5;
+
+    /** @ODM\Int */
+    private $ratingsCount = 0;
+
+    /** @ODM\Int */
+    private $ratingsSummary = 0;
+
+    /** @ODM\Int */
+    private $yesterdayComments = 0;
+
+    /** @ODM\String */
+    private $usedSkin = 'default';
+
+    public function __construct()
+    {
+        $this->regId = Uuid::uuid4();
+        $this->regDate = time();
+    }
 
     public function setName($name)
     {
